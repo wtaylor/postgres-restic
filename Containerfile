7f77669ad889db0efed707778d5348aa6377e14f
William Taylor <me@willtaylor.info>
2026-02-28 14:53:00 +0000

    Add dynamic DUMP_ROOT variable

--- Containerfile
+++ Containerfile
@@ -1,5 +1,7 @@
 FROM debian:trixie-20260223-slim
 
+LABEL org.opencontainers.image.source=https://github.com/wtaylor/postgres-restic
+
 RUN apt-get update && apt-get install -y \
   restic \
   postgresql-client \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
+
+export DUMP_ROOT_VAR="DUMP_ROOT"
+export DUMP_ROOT="${!DUMP_ROOT_VAR:-/pg_dump}"
+
+mkdir -p "$DUMP_ROOT"
 
 for i in {1..5}; do
-  export HOSTNAME_VAR="HOSTNAME_$i"
   export PGHOST_VAR="PGHOST_$i"
   export PGPASSWORD_VAR="PGPASSWORD_$i"
   export PGPORT_VAR="PGPORT_$i"
   export PGUSER_VAR="PGUSER_$i"
   export RESTIC_REPOSITORY_VAR="RESTIC_REPOSITORY_$i"
 
-  export HOST="${!HOSTNAME_VAR:-${!PGHOST_VAR}}"
   export PGHOST="${!PGHOST_VAR}"
   export PGPASSWORD="${!PGPASSWORD_VAR}"
   export PGPORT="${!PGPORT_VAR:-5432}"
@@ -33,7 +36,7 @@ for i in {1..5}; do
 
   # Wait for PostgreSQL to become available.
   COUNT=0
-  until psql -l >/dev/null 2>&1; do
+  until psql -l; do
     if [[ "$COUNT" == 0 ]]; then
       echo "Waiting for PostgreSQL to become available..."
     fi
@@ -44,24 +47,22 @@ for i in {1..5}; do
     echo "Waited $COUNT seconds."
   fi
 
-  mkdir -p "/pg_dump"
-
   # Dump individual databases directly to restic repository.
   DBLIST=$(psql -d postgres -q -t -c "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres', 'rdsadmin', 'template0', 'template1')")
   for dbname in $DBLIST; do
     echo "Dumping database '$dbname'"
-    pg_dump -Fc --file="/pg_dump/$dbname.dump" "$dbname"
+    pg_dump -Fc --file="$DUMP_ROOT/$dbname.dump" "$dbname"
   done
 
   echo "Sending database dumps to $RESTIC_REPOSITORY..."
-  while ! restic backup "/pg_dump"; do
+  while ! restic backup "$DUMP_ROOT"; do
     echo "Sleeping for 10 seconds before retry..."
     sleep 10
   done
 
   echo "Finished sending database dumps to $RESTIC_REPOSITORY"
 
-  rm -rf "/pg_dump"
+  rm -rf "$DUMP_ROOT/*"
 done
 
 echo 'Finished backup successfully'
